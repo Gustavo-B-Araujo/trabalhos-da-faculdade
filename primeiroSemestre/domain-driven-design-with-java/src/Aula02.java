cc818787309933013bbd8acbc326732d002bed47
Gustavo-B-Araujo <gubarrios@icloud.com>
2025-03-13 14:29:32 -0300

    Primeiro commit

---
+++ primeiroSemestre/domain-driven-design-with-java/src/Aula02.java
@@ -0,0 +1,17 @@
+public class Aula02 {
+    public static void main(String[] args) {
+        int idade = 27;
+        double salario = 1000.00;
+        char sexo = 'M';
+        boolean casado = true;
+        String nome = "Gustavo";
+
+        System.out.println(idade);
+        System.out.println(salario);
+
+        double salarioReajustado = salario * 1.1;
+        System.out.println(salarioReajustado);
+
+        System.out.println(salario / 100 * 10);
+    }
+}
---
+++ primeiroSemestre/domain-driven-design-with-java/src/Exercicio01.java
@@ -0,0 +1,11 @@
+public class Exercicio01 {
+    public static void main(String[] args){
+        double simples = 200000.00;
+        double bitrem = 400000.00;
+        double furgao = 100000.00;
+        double vuc = 100000.00;
+        double total = simples + bitrem + furgao + vuc;
+
+        System.out.println(total);
+    }
+}
